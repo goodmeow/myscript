1aa0b8b94b474c6e9b7747eb8e7f6a63c4b7a5f9
goodmeow <aarunalr@nusantararom.org>
2022-06-18 16:39:19 +0700

    change gdrive binary

--- setup/gdrive_install.sh
+++ setup/gdrive_install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-wget https://github.com/ProtoDump/rel/releases/download/gdrive/gdrive
+wget https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
 chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 rm -rf gdrive
